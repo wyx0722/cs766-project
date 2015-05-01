ae2eb497a36af9fbe63bbc25c5534cfe8808824d
Chris <cbodden@cs.wisc.edu>
2015-05-01 16:02:52 -0500

    changes to lda

--- build_LDA_model.m
+++ build_LDA_model.m
@@ -17,6 +17,31 @@ function [predicted_labels] = build_LDA_model(train_data, testing_data, L)
 
 % Run LDA estimation on training (need to adjust to use lda-c)
 
+% build data files in correct format for train/test sets (unfortunately nasty)
+training_filename = 'lda-c/model/training_data.dat';
+testing_filename = 'lda-c/model/testing_data.dat';
+word_ind = 1:C;
+formatSpec = ' %i:%f';
+
+train_file = fopen(training_filename,'w');
+for train_doc = 1:V_train
+    fprintf(train_file, '%i', C); % 1st number
+    fprintf(train_file, formatSpec, [word_ind; train_data(train_doc,:)]); % data
+    fprintf(train_file, '\n'); % endline
+end
+fclose(train_file);
+
+test_file = fopen(testing_filename,'w');
+for test_doc = 1:V_test
+    fprintf(test_file, '%i', C); % 1st number
+    fprintf(test_file, formatSpec, [word_ind; testing_data(test_doc,:)]); % data
+    fprintf(test_file, '\n'); % endline
+end
+fclose(test_file);
+
+run_string = ['./lda-c/build/lda est estimate ' num2str(L) ' lda-c/settings.txt ' training_filename ' random lda-c/model/'];
+s = system(run_string);
+
 % Run LDA inference on test data
 
 end
