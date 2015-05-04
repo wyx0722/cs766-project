56656f561687493bf7d4dd5876e298b35cbeae2e
Chris <cbodden@cs.wisc.edu>
2015-05-04 14:16:47 -0500

    added libsvm to repo

--- test_20150503/test_classification.m
+++ test_20150503/test_classification.m
@@ -4,12 +4,15 @@ load('labels_train');
 load('features');
 load('features_train');
 
+addpath('C:\Users\cbodden\Documents\GitHub\cs766-project\libsvm-3.20\matlab');
+
 %% train one-class SVM
 model = svmtrain(double(labels_train), features_train, '-s 2 -t 2');
 [~, ~, dec_values] = svmpredict(double(labels), features, model);
 norm_dec_values = 1 - mat2gray(dec_values);
 
 %% plot ROC curve
+logical_labels = norm_dec_values >= 0.50;
 plotroc(labels', norm_dec_values');
 [tpr,fpr,thresholds] = roc(labels', norm_dec_values');
 area = 0;
@@ -17,3 +20,7 @@ for i = 1 : length(fpr) - 1
     area = area + (fpr(i + 1) - fpr(i)) * tpr(i);
 end
 area
+
+accuracy = sum(labels == logical_labels) / length(labels) * 100
+
+rmpath('C:\Users\cbodden\Documents\GitHub\cs766-project\libsvm-3.20\matlab')
