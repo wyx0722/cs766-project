9c4d32d47d49d4e08e74d16e083fe3b51781c49e
Chris <cbodden@cs.wisc.edu>
2015-05-05 02:12:57 -0500

    updated compile all features script

--- make_full_feature_data.m
+++ make_full_feature_data.m
@@ -1,3 +1,4 @@
+clear
 videoDirectoy = '/media/Data/School/CS766/Final Project/data/features/UMN/';
 videos = {'01', '02', '03', '04', '05', '06',...
     '07', '08', '09', '10', '11'};
@@ -23,40 +24,37 @@ clip_raw_features.MBH = {}; % 1:96 -> x, 97:192 -> y
 clip_raw_features.SocialForce = {};
 clip_raw_features.labels = {};
 
+%social force comp
+[~, ~, ~, ~, Sf] = compute_interaction_force(videoDirectoy);
+
+%store
+current_index = 0;
 for i = 1 : num_videos
     files = dir([videoDirectoy videos{i}, '_*.txt']);
     num_files = length(files);
+    clip_raw_features.clipIndexInfo{i} = 1:num_files; % length = # videos, clipIndexInfo{i} = array of clip numbers
     
     for j = 1 : num_files
+        current_index = current_index + 1;
         temp_clip = load([videoDirectoy files(j).name]);
         
-        clip_raw_features.clipIndexInfo{i} = 1:num_files; % [video #, clip #]
-        clip_raw_features.rawFileNames{i,j} = files(j).name;
-        clip_raw_features.generalInfo{i,j} = temp_clip(:,genInfoIndices); % i=video, j=clip
-        clip_raw_features.denseTrajectories{i,j} = temp_clip(:,denseTrajIndices);
-        clip_raw_features.HOG{i,j} = temp_clip(:,HOGIndices);
-        clip_raw_features.HOF{i,j} = temp_clip(:,HOFIndices);
-        clip_raw_features.MBH{i,j} = temp_clip(:,MBHIndices);
+        clip_raw_features.rawFileNames{current_index} = files(j).name;
+        clip_raw_features.generalInfo{current_index} = temp_clip(:,genInfoIndices); % i=video, j=clip
+        clip_raw_features.denseTrajectories{current_index} = temp_clip(:,denseTrajIndices);
+        clip_raw_features.HOG{current_index} = temp_clip(:,HOGIndices);
+        clip_raw_features.HOF{current_index} = temp_clip(:,HOFIndices);
+        clip_raw_features.MBH{current_index} = temp_clip(:,MBHIndices);
+        clip_raw_features.SocialForce{current_index} = Sf{current_index}(:,:,3);
         
         if ( j <= transitions(i) )
-            clip_raw_features.labels{i,j} = 1; % normal is a positive example
+            clip_raw_features.labels{current_index} = 1; % normal is a positive example
         else
-            clip_raw_features.labels{i,j} = -1; % abnormal is negative
+            clip_raw_features.labels{current_index} = -1; % abnormal is negative
         end
         clear temp_clip
     end
     clear num_files files
 end
-
-%social force comp
-[~, ~, ~, ~, Sf] = compute_interaction_force(videoDirectoy);
-current_index = 0;
-for i = 1:length(clip_raw_features.clipIndexInfo)
-    for j = clip_raw_features.clipIndexInfo{i}
-        current_index = current_index + 1;
-        index = clip_raw_features.clipIndexInfo{i}
-        clip_raw_features.SocialForce{i,j} = Sf{current_index}(:,:,3);
-    end
-end
 clear Sf
-save('all_features.mat',clip_raw_features);
+
+save('all_features.mat','clip_raw_features');
